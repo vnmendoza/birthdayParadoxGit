ef53c74dbc5f5e6eadd906d07cd7a4209a96db29
VNM <vnmendoza94@gmail.com>
2017-09-07 22:42:55 -0700

    finished project

--- birthdayParadox/Source.cpp
+++ birthdayParadox/Source.cpp
@@ -2,35 +2,66 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 using namespace std;
+vector<int> createBdays(int);
+int matchBday(vector<int>);
 
 int main()
 {
-	vector<int> bdays;
 	
-	srand(time(0));
-	int x;
+	srand((unsigned) time(0));
+	double ctr;
+	double trials = 100;
 	//# of people in room
-	for (int n = 5; n <= 10; n += 5)
+	for (int n = 5; n <= 100; n += 5)
 	{
+		ctr = 0;
 		
 		cout << "Group of " << n << endl;
 		//number of trials
-		for (int i = 0; i < 10; i++)
+		for (int tri = 0; tri < trials; tri++)
 		{
-			cout << "Trial " << i + 1 << ": "
-				<< x << " people shared birthdays";
-			//number of birthdays that match
-			for (int i = 0; i < n; i++)
-			{
-				int b = rand() % 367;
-				bdays.push_back(b);
-				cout << bdays[i] << endl;
-			}
+			vector <int> bdays = createBdays(n);
+			ctr += matchBday(bdays);
+			
+			
 		}
-		
+		cout << ctr << "/"<<trials<<" trial(s) had shared names. \n";
 
 	}
 	
+	
+}
+
+//generates random bdays
+vector<int> createBdays (int ppl)
+{
+	vector <int> Cbdays;
+
+	for (int i = 0; i < ppl; i++)
+	{
+		int b = rand() % 366;
+		Cbdays.push_back(b);
+	}
+	return Cbdays;
+}
+
+//matches bdays on same day
+int matchBday(vector<int> birthdays)
+{
+	int match = 0;
+	sort(birthdays.begin(), birthdays.end());
+	for (int j = 0; j < birthdays.size() - 1 && match == 0; j++)
+	{
+		for (int k = j + 1; k < birthdays.size() && match == 0; k++)
+		{
+			if (birthdays[k] == birthdays[j])
+			{
+				match++;
+			}
+		}
+	}
+	return match;
 	
 }
